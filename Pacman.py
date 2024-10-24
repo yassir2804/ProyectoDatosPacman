419ac8c4d3f9c62826d54129513b392bd5d3a664
Angel QJ <angel.quesada.jimenez@est.una.ac.cr>
2024-10-23 21:47:00 -0600

    Arreglos movimientos pacman

--- Pacman.py
+++ Pacman.py
@@ -5,7 +5,6 @@ from Constantes import *
 class Pacman(object):
     def __init__(self,nodo):
         self.nombre = PACMAN
-        #self.posicion = Vector1(200, 400)
         self.direcciones = {STOP: Vector1(0,0), ARRIBA: Vector1(0,-1), ABAJO: Vector1(0,1), IZQUIERDA: Vector1(-1,0), DERECHA: Vector1(1,0)}
         self.direccion = STOP
         self.velocidad = 100* ANCHOCELDA/16 #Esto es una formula que sirve para que en cualquier formato de pantalla el pacman vaya a una velocidad considerable
@@ -15,8 +14,40 @@ class Pacman(object):
         self.setPosicion()
         self.blanco=nodo
         
+        
     def setPosicion(self):
         self.posicion = self.nodo.posicion.copiar()
+
+    def actualizar(self, dt):
+        self.posicion += self.direcciones[self.direccion]*self.velocidad * dt
+        direc = self.entradaTeclado()
+        #self.nodo= self.getNuevoBlanco(self.direccion)
+        #self.setPosicion()
+        if self.overshotTarget():
+            self.nodo = self.blanco
+            self.blanco = self.getNuevoBlanco(self.direccion)
+            if self.blanco is not self.nodo:
+                self.direccion =  direc
+            else:
+                self.blanco = self.getNuevoBlanco(direc)
+
+            if self.blanco is self.nodo:
+                self.direccion = STOP
+            self.setPosicion()
+        else:
+            if self.direccionOpuesta(self.direccion):
+             self.direccionReversa()
+
+    def validarDireccion(self, direccion):
+        if direccion is not STOP:
+            if self.nodo.vecinos[direccion] is not None:
+                return True
+        return False
+
+    def getNuevoBlanco(self, direccion):
+        if self.validarDireccion(direccion):
+            return self.nodo.vecinos[direccion]
+        return self.nodo
 
 
     def entradaTeclado(self):
@@ -30,40 +61,29 @@ class Pacman(object):
         if teclaPresionada[K_RIGHT]:
             return DERECHA
         return STOP
-    def actualizar(self, dt):
-        self.posicion += self.direcciones[self.direccion]*self.velocidad * dt
-        self.direccion = self.entradaTeclado()
-        #self.nodo= self.getNuevoBlanco(self.direccion)
-        #self.setPosicion()
-        if self.overshotTarget():
-            self.nodo = self.blanco
-            self.blanco = self.getNuevoBlanco(direccion)
-            if self.blanco is not self.nodo:
-                self.direccion = direccion
-            else:
-                self.direction = STOP
-            self.setPosition()
-
-    def validarDireccion(self, direccion):
-        if direccion is not STOP:
-            if self.nodo.vecinos[direccion] is not None:
-                return True
-        return False
-
-    def getNuevoBlanco(self, direccion):
-        if self.validarDireccion(direccion):
-            return self.nodo.vecinos[direccion]
-        return self.nodo
-    
-    def render(self, pantalla):
-        p = self.posicion.entero()
-        pygame.draw.circle(pantalla, self.color, p, self.radio)
 
     def overshotTarget(self):
         if self.blanco is not None:
             vec1 = self.blanco.posicion- self.nodo.posicion
             vec2 = self.posicion - self.nodo.posicion
-            nodo2Blanco = vec1.magnitudeSquared()
-            nodo2Self = vec2.magnitudeSquared()
-            return nodo2Self >= nodo2Blanco
+            nodo2Blanco = vec1.magnitudCuadrada()
+            nodo2Self = vec2.magnitudCuadrada()
+            TOLERANCIA = 0.0001
+            return nodo2Self >= nodo2Blanco - TOLERANCIA
         return False
+
+    def direccionReversa(self):
+        self.direccion *= -1
+        temp = self.nodo
+        self.nodo = self.blanco
+        self.blanco = temp
+
+    def direccionOpuesta(self, direccion):
+        if direccion is not STOP:
+            if direccion == self.direccion * -1:
+                return True
+        return False
+
+    def render(self, pantalla):
+        p = self.posicion.entero()
+        pygame.draw.circle(pantalla, self.color, p, self.radio)
